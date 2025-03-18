72102e6883d076f8742f1320ceade5994535d34a
Godbless Umukoro <61124647+UmukoroG@users.noreply.github.com>
2025-03-18 15:07:54 -0500

    Time: 0 ms (100%), Space: 13.4 MB (7.57%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,16 +1,23 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int m=matrix.size();
-        int n=matrix[0].size();
-        for(int i=0; i<m; i++){
-            for(int j=0; j<n;j++){
-                if(matrix[i][j]==target){
-                    return true;
-                    break;
-                }
+        int m = matrix.size();
+        int n = matrix[0].size();
+        int left = 0, right = m * n - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            int midValue = matrix[mid / n][mid % n]; // Convert mid index to 2D coordinates
+
+            if (midValue == target) {
+                return true;
+            } else if (midValue < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
             }
         }
+        
         return false;
     }
 };
