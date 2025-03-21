915cc4c85208b991d5ac30ca3dbe34b2dd15d969
Godbless Umukoro <61124647+UmukoroG@users.noreply.github.com>
2025-03-21 02:43:49 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
@@ -4,15 +4,18 @@ public:
         if(nums.empty()) return -1;
         int l=0;
         int r=nums.size()-1;
+        int minimum=INT_MAX;
         while(l<r){
             int mid=l+(r-l)/2;
             if(nums[mid]>nums[r]){
                 l=mid+1;
+                minimum=min(minimum,nums[mid]);
             }
             else{
                 r=mid;
+                minimum=min(minimum, nums[mid]);
             }
         }
-        return nums[l];
+        return minimum;
     }
 };
