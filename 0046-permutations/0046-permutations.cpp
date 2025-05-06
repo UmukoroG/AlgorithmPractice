6d99a1e382af477260b198815f830a91f717f658
Godbless Umukoro <61124647+UmukoroG@users.noreply.github.com>
2025-05-06 12:35:05 -0500

    Time: 4 ms (14.86%), Space: 10.6 MB (49.17%) - LeetHub

---
+++ 0046-permutations/0046-permutations.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> res;
+        vector<int> perm;
+        vector<bool> used(nums.size(), false); // {false, false, false}
+        dfs(nums, res, perm,used);
+        return res;
+
+        
+    }
+private:
+    void dfs(vector<int>& nums, vector<vector<int>>& res, vector<int>& perm, vector<bool>& used){
+        if(perm.size()==nums.size()){//base case
+            res.push_back(perm);
+            return;
+        }
+
+        for(int i=0; i<nums.size(); i++){
+            if(used[i]==true) continue;
+
+            perm.push_back(nums[i]);
+            used[i]=true;
+            dfs(nums, res, perm, used);
+
+            perm.pop_back();
+            dfs(nums, res, perm, used);
+            used[i]=false;
+        }
+    }
+};
