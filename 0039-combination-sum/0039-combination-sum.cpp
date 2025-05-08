64cde4b55ca31784d01ffb8b4147d17df062ffd5
Godbless Umukoro <61124647+UmukoroG@users.noreply.github.com>
2025-05-08 13:30:04 -0500

    Time: 4 ms (43.03%), Space: 13.9 MB (91.22%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>> res;
+        vector<int> combn;
+        dfs(candidates,res, combn, target, 0);
+        return res;
+    }
+private:
+    void dfs(vector<int>& candidates, vector<vector<int>>& res, vector<int>& combn, int target, int i){
+        if(i>= candidates.size()) return; //i could exceeds the bounds
+        
+        int sum = accumulate(combn.begin(), combn.end(),0);
+        if(sum==target){//check if combn sum == target
+            res.push_back(combn);
+            return;
+        }
+        else if(sum>target){
+            return;
+        }
+
+        combn.push_back(candidates[i]); //include i
+        dfs(candidates, res, combn, target, i);
+
+        combn.pop_back();//don't include i
+        dfs(candidates, res, combn, target, i+1);
+    }
+};
