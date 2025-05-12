dd82314057c4db0ea416dda6f708873f7c81c7eb
Godbless Umukoro <61124647+UmukoroG@users.noreply.github.com>
2025-05-12 13:33:23 -0500

    Time: 836 ms (25.91%), Space: 11.6 MB (36.24%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,11 +1,8 @@
 class Solution {
 public:
-   int ROWS;
-   int COLS;
-   set<pair<int,int>> path;
     bool exist(vector<vector<char>>& board, string word) {
-        ROWS = board.size();
-        COLS = board[0].size();
+        int ROWS = board.size();
+        int COLS = board[0].size();
 
         for(int rows=0; rows<ROWS; rows++){
             for(int cols=0; cols<COLS; cols++){
@@ -21,17 +18,20 @@ private:
         if(i == word.size()) 
             return true; //track we have reached the end of the search
         
-        if(rows < 0 || cols<0 || rows>=ROWS || cols>=COLS || 
-            board[rows][cols]!=word[i] || path.count({rows,cols})){
+        if(rows < 0 || cols<0 || rows>=board.size() || cols>=board[0].size() || 
+            board[rows][cols]!=word[i]){
             return false;
         }
-        path.insert({rows,cols});//insert path once we visit it
+        char temp = board[rows][cols];
+        board[rows][cols]='#'; //marked as visited
+
+        //visits all required paths
         bool res = dfs(board,word,rows+1, cols,i+1)||
                     dfs(board,word,rows-1, cols,i+1)||
                     dfs(board,word,rows, cols+1,i+1)||
                     dfs(board,word,rows, cols-1,i+1);
                     
-        path.erase({rows,cols}); //remove path to redo backtracking
+        board[rows][cols]=temp; //restore the original value;
 
         return res;
     }
