979b05cf511523afc902064d9db454a6be56ef14
Godbless Umukoro <61124647+UmukoroG@users.noreply.github.com>
2025-11-04 12:29:19 -0600

    Time: 0 ms (100%), Space: 8.4 MB (12.49%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -1,18 +1,11 @@
 class Solution {
 public:
     int hammingWeight(int n) {
-        string res = "";
-
+        int count =0;
         while(n!=0){
-            if(n%2==1) res+='1';
-            else{
-                res+='0';
-            }
-            n=n/2;
+            count+=n&1; //add 1 if the last bit is 1
+            n>>=1; //right shift to check the next bit
         }
-        reverse(res.begin(), res.end());
-        int setBit = count(res.begin(), res.end(),'1');
-
-        return setBit;
+        return count;
     }
 };
