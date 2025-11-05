033a9bb539685825a765ff443e004a0edf0bfd32
Godbless Umukoro <61124647+UmukoroG@users.noreply.github.com>
2025-11-05 16:54:51 -0600

    Time: 0 ms (100%), Space: 10.7 MB (57.76%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -1,28 +1,10 @@
 class Solution {
 public:
     vector<int> countBits(int n) {
-        vector<int> res;
-
-        for(int i=0; i<=n;i++){
-            int x = cnt(i);
-            res.push_back(x);
+        vector<int> res(n+1,0);
+        for(int i=0; i<=n; i++){
+            res[i] = res[i>>1] + (i&1);
         }
-
         return res;
-    }
-
-    int cnt(int n){{
-        string res ="";
-        while(n!=0){
-            if(n%2==1) res+='1';
-            else{
-                res+='0';
-            }
-            n=n/2;
-        }
-        int numofOnes = count(res.begin(), res.end(),'1');
-        return numofOnes;
-    }
-
     }
 };
