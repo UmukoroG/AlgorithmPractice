a9ac05c18046d79fd5daf16d14e2191317da947a
Godbless Umukoro <61124647+UmukoroG@users.noreply.github.com>
2025-11-27 00:07:32 -0600

    Time: 8 ms (95.97%), Space: 24.9 MB (76.48%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -3,20 +3,19 @@ public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         if(strs.empty()) return {{}};
 
+        unordered_map<string, vector<string>> mp;
         vector<vector<string>> res;
 
-        unordered_map<string, vector<string>> str;
-
-        for(auto& i : strs){
-            string s= i;
+        for(auto& i:strs){
+            string s = i;
             sort(s.begin(), s.end());
-            str[s].push_back(i);
-            
+            mp[s].push_back(i);
         }
 
-        for(auto& pair : str){
+        for(auto& pair: mp){
             res.push_back(pair.second);
         }
+
         return res;
     }
 };
